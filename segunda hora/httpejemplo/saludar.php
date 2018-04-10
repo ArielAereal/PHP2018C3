24f4080200085ba9e2cc76f86607b32ceb79733d
alumno <alumno>
2018-04-09 22:01:30 -0300

    final de la ultima hora, postman y GET POST PUT DELETE

---
+++ segunda hora/httpejemplo/saludar.php
@@ -0,0 +1,37 @@
+<?php
+
+//echo "hola";
+
+
+
+var_dump($_REQUEST);
+
+var_dump($_GET);
+
+var_dump($_POST);
+
+$nom = $_GET['nombre'];
+
+$leg = $_POST['legajo'];
+
+// que funcione hora uno
+
+// gestion tiene que saber el metodo por que viene "Detectar metodo"
+
+// le paso un solo parametro (patente)
+
+// misma direccion, cambia el metodo y hace cosas distintas
+
+// PUT modificar
+
+// DELETE eliminar
+
+// GET traer
+
+// POST insertar
+
+// ver $_SERVER['HTTP_REFERER']
+
+// importante para los parciales
+
+?>
