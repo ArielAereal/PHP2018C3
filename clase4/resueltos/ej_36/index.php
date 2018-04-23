5317cb5b9a21f93f7d95bb3e2c69adf7d290b565
Ariel David Cuevas <ariel257cuevas@yahoo.com.ar>
2018-04-23 16:53:24 -0300

    ejercicio 36
    resuelto

---
+++ clase4/resueltos/ej_36/index.php
@@ -0,0 +1,67 @@
+<?php
+
+
+
+
+echo "<table border='2px' solid>";
+
+echo "<caption>Fotorrafías</caption>";
+
+
+
+echo "<tbody>";
+
+echo "<tr>";
+echo '<form action="lafot.php" method="post">';
+echo "<td><input type='text' value='Sol.jpg' name='foto' hidden><button type='submit'><img src='http://localhost/prog/resueltos/ej_36/fotos/Sol.jpg' width='100px' height='100px'></button></td> ";
+echo "</form>";
+echo '<form action="lafot.php" method="post">';
+echo "<td><input type='text' value='Coati.jpg' name='foto' hidden><button type='submit'><img src='http://localhost/prog/resueltos/ej_36/fotos/Coati.jpg' width='100px' height='100px'></button></td> ";
+echo "</form>";
+echo "</tr>";
+
+echo "<tr>";
+echo '<form action="lafot.php" method="post">';
+echo "<td><input type='text' value='ElPalmar.jpg' name='foto' hidden><button type='submit'><img src='http://localhost/prog/resueltos/ej_36/fotos/ElPalmar.jpg' width='100px' height='100px'></button></td> ";
+echo "</form>";
+echo '<form action="lafot.php" method="post">';
+echo "<td><input type='text' value='HockeyLasLeonas.jpg' name='foto' hidden><button type='submit'><img src='http://localhost/prog/resueltos/ej_36/fotos/HockeyLasLeonas.jpg' width='100px' height='100px'></button></td> ";
+echo "</form>";
+
+echo "</tr>";
+
+echo "<tr>";
+echo '<form action="lafot.php" method="post">';
+echo "<td><input type='text' value='Jaguar.jpg' name='foto' hidden><button type='submit'><img src='http://localhost/prog/resueltos/ej_36/fotos/Jaguar.jpg' width='100px' height='100px'></button></td> ";
+echo "</form>";
+echo '<form action="lafot.php" method="post">';
+echo "<td><input type='text' value='JuanManuelFangio.png' name='foto' hidden><button type='submit'><img src='http://localhost/prog/resueltos/ej_36/fotos/JuanManuelFangio.png' width='100px' height='100px'></button></td> ";
+echo "</form>";
+
+echo "</tr>";
+echo "<tr>";
+echo '<form action="lafot.php" method="post">';
+echo "<td><input type='text' value='LagoPuelo.jpg' name='foto' hidden><button type='submit'><img src='http://localhost/prog/resueltos/ej_36/fotos/LagoPuelo.jpg' width='100px' height='100px'></button></td> ";
+echo "</form>";
+echo '<form action="lafot.php" method="post">';
+echo "<td><input type='text' value='Ñandu.jpg' name='foto' hidden><button type='submit'><img src='http://localhost/prog/resueltos/ej_36/fotos/Ñandu.jpg' width='100px' height='100px'></button></td> ";
+echo "</form>";
+
+echo "</tr>";
+echo "<tr>";
+echo '<form action="lafot.php" method="post">';
+echo "<td><input type='text' value='SelvaMisionera.jpg' name='foto' hidden><button type='submit'><img src='http://localhost/prog/resueltos/ej_36/fotos/SelvaMisionera.jpg' width='100px' height='100px'></button></td> ";
+echo "</form>";
+echo '<form action="lafot.php" method="post">';
+echo "<td><input type='text' value='Tucan.jpg' name='foto' hidden><button type='submit'><img src='http://localhost/prog/resueltos/ej_36/fotos/Tucan.jpg' width='100px' height='100px'></button></td> ";
+echo "</form>";
+echo "</tr>";
+echo "</tbody>";
+
+echo "</table>";
+
+
+
+
+
+?>
---
+++ clase4/resueltos/ej_36/lafot.php
@@ -0,0 +1,12 @@
+<?php
+
+//var_dump($_POST);
+
+$mostrar = $_POST['foto'];
+
+echo '<img src=http://localhost/prog/resueltos/ej_36/fotos/'.$mostrar.'>';
+
+echo '<a href="index.php">Volver</a>';
+
+
+?>
